49b2b4b6ce856e31da135525df59067085986bf8
mario-r99 <mario.srx99@gmail.com>
2022-03-20 02:48:55 +0100

    Initial commit

---
+++ app/src/main/java/com/escape99/minimalpiano/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.escape99.minimalpiano
+
+import android.media.AudioAttributes
+import android.media.SoundPool
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var headingButton: Button
+    private lateinit var soundPool: SoundPool
+    private var sound1: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_GAME)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+        soundPool = SoundPool.Builder()
+            .setMaxStreams(10)
+            .setAudioAttributes(audioAttributes)
+            .build()
+        sound1 = soundPool.load(this, R.raw.acoustic_grand_piano_a3, 1)
+
+        headingButton = findViewById(R.id.headingButton)
+
+        headingButton.setOnTouchListener { view, motionEvent ->
+            if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                soundPool.play(sound1, 1F, 1F, 0, 0, 1F)
+            }
+            return@setOnTouchListener view.onTouchEvent(motionEvent)
+        }
+
+    }
+
+}

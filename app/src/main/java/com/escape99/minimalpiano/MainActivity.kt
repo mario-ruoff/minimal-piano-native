4f3cd0a75f133b916fff80d67dbc344b5f531394
mario-r99 <mario.srx99@gmail.com>
2022-11-14 10:19:18 +0100

    Add sound

--- app/src/main/java/com/escape99/minimalpiano/MainActivity.kt
+++ app/src/main/java/com/escape99/minimalpiano/MainActivity.kt
@@ -2,34 +2,57 @@ package com.escape99.minimalpiano
 
 import android.media.AudioAttributes
 import android.media.SoundPool
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.core.view.children
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var soundPool: SoundPool
-    private var sound1: Int = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initializeAudio()
         setFullscreenMode()
         setContentView(R.layout.activity_main)
+        initializeAudio()
     }
 
     private fun initializeAudio() {
+        // Set up audio builder
         val audioAttributes = AudioAttributes.Builder()
             .setUsage(AudioAttributes.USAGE_GAME)
             .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
             .build()
-        soundPool = SoundPool.Builder()
+        val soundPool = SoundPool.Builder()
             .setMaxStreams(10)
             .setAudioAttributes(audioAttributes)
             .build()
-        sound1 = soundPool.load(this, R.raw.acoustic_grand_piano_a3, 1)
+
+        // Load sounds from keys defined in xml
+        val keys: ConstraintLayout = findViewById(R.id.keys)
+        for (key in keys.children) {
+            if (key.id != R.id.guideline) {
+                val keyName = resources.getResourceEntryName(key.id)
+                val soundId = resources.getIdentifier("acoustic_grand_piano_$keyName", "raw", applicationContext.packageName)
+                val sound = soundPool.load(this, soundId, 1)
+                key.setOnTouchListener { view, event ->
+                    when(event.action) {
+                        MotionEvent.ACTION_DOWN -> {
+                            soundPool.play(sound, 2f, 2f, 0, 0, 1f)
+                        }
+                        MotionEvent.ACTION_MOVE -> { }
+                        MotionEvent.ACTION_UP -> {
+                            view.performClick()
+                        }
+                    }
+                    true
+                }
+            }
+        }
     }
 
     private fun setFullscreenMode() {
